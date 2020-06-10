5b5b53156a2ab8f0d81f67ca76791fe0cf092723
aecoffeenk <huanlianglee@gmail.com>
2020-06-10 17:09:26 +0800

    update_db

--- source/scs.sql
+++ source/scs.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- 主機： 127.0.0.1
--- 產生時間： 2020-06-05 16:24:30
--- 伺服器版本： 10.4.11-MariaDB
--- PHP 版本： 7.4.6
+-- Host: 127.0.0.1
+-- Generation Time: Jun 10, 2020 at 11:08 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `scs`
+-- Database: `new_scs`
 --
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `employee`
+-- Table structure for table `employee`
 --
 
 CREATE TABLE `employee` (
@@ -35,7 +35,7 @@ CREATE TABLE `employee` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 傾印資料表的資料 `employee`
+-- Dumping data for table `employee`
 --
 
 INSERT INTO `employee` (`emp_id`, `emp_name`, `tel.`, `rating`) VALUES
@@ -53,7 +53,29 @@ INSERT INTO `employee` (`emp_id`, `emp_name`, `tel.`, `rating`) VALUES
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `project`
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `loc_pid` char(10) NOT NULL,
+  `loc_addr` varchar(30) NOT NULL,
+  `lat` float NOT NULL,
+  `lng` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`loc_pid`, `loc_addr`, `lat`, `lng`) VALUES
+('P00001', '407台中市西屯區文華路121-12號', 24.1801, 120.646),
+('P00002', '407台中市西屯區西屯路二段236號', 24.1691, 120.648),
+('P00003', '40743台中市西屯區福星路525號', 24.1772, 120.645);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
 --
 
 CREATE TABLE `project` (
@@ -61,36 +83,35 @@ CREATE TABLE `project` (
   `P_name` varchar(30) NOT NULL,
   `start_date` datetime(6) NOT NULL,
   `end_date` datetime(6) NOT NULL,
-  `location` varchar(30) NOT NULL,
   `detail` text NOT NULL,
   `num_req` int(4) DEFAULT NULL,
   `client` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 傾印資料表的資料 `project`
+-- Dumping data for table `project`
 --
 
-INSERT INTO `project` (`P_id`, `P_name`, `start_date`, `end_date`, `location`, `detail`, `num_req`, `client`) VALUES
-('P00001', '星火公司周年派對', '2020-05-30 13:10:00.000000', '2020-05-30 17:00:00.000000', '台中市西屯區文華路9000號 13F', '會場約100坪。 會場入口處須要擺置8架花束，前方兩側需要各一普通聚光燈以及大型音響，後方兩側各一普通音響，後方中間架設拍攝平台。', 5, '邱小源'),
-('P00002', '張小伊 陳小軍 婚禮', '2020-06-10 17:00:00.000000', '2020-06-10 21:30:00.000000', '台中市西屯區西屯路二段1990號 10F', '會場約60坪。 會場四周處須要擺置各4架花束，前方兩側需要各一普通音響，後方兩側各一普通音響，後方中間架設拍攝平台。', 3, '陳小軍'),
-('P00003', '多視界影視公司 開業晚宴', '2020-06-15 17:00:00.000000', '2020-06-15 22:45:00.000000', '台中市西屯區福星路3000號 4F', '會場約80坪。 會場入口處須以氣球裝飾，前方兩側需要各一普通聚光燈以及普通音響，後方兩側各一普通音響，後方中間架設拍攝平台。', 2, '光小中');
+INSERT INTO `project` (`P_id`, `P_name`, `start_date`, `end_date`, `detail`, `num_req`, `client`) VALUES
+('P00001', '星火公司周年派對', '2020-05-30 13:10:00.000000', '2020-05-30 17:00:00.000000', '會場約100坪。 會場入口處須要擺置8架花束，前方兩側需要各一普通聚光燈以及大型音響，後方兩側各一普通音響，後方中間架設拍攝平台。', 5, '邱小源'),
+('P00002', '張小伊 陳小軍 婚禮', '2020-06-10 17:00:00.000000', '2020-06-10 21:30:00.000000', '會場約60坪。 會場四周處須要擺置各4架花束，前方兩側需要各一普通音響，後方兩側各一普通音響，後方中間架設拍攝平台。', 3, '陳小軍'),
+('P00003', '多視界影視公司 開業晚宴', '2020-06-15 17:00:00.000000', '2020-06-15 22:45:00.000000', '會場約80坪。 會場入口處須以氣球裝飾，前方兩側需要各一普通聚光燈以及普通音響，後方兩側各一普通音響，後方中間架設拍攝平台。', 2, '光小中');
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `project_member`
+-- Table structure for table `project_member`
 --
 
 CREATE TABLE `project_member` (
-  `Pid` char(10) DEFAULT NULL,
+  `Pid` char(10) NOT NULL,
   `emp_id` char(10) NOT NULL,
   `lead.flag` tinyint(1) NOT NULL,
   `detail` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 傾印資料表的資料 `project_member`
+-- Dumping data for table `project_member`
 --
 
 INSERT INTO `project_member` (`Pid`, `emp_id`, `lead.flag`, `detail`) VALUES
@@ -99,43 +120,56 @@ INSERT INTO `project_member` (`Pid`, `emp_id`, `lead.flag`, `detail`) VALUES
 ('P00001', 'E00003', 0, NULL),
 ('P00001', 'E00004', 0, NULL),
 ('P00001', 'E00005', 0, NULL),
-('P00002', 'E00006', 1, '負責領導，監督工作，與客戶溝通'),
 ('P00001', 'E00007', 0, '負責布置門口部分'),
+('P00002', 'E00006', 1, '負責領導，監督工作，與客戶溝通'),
 ('P00002', 'E00008', 0, NULL),
 ('P00003', 'E00009', 0, NULL),
 ('P00003', 'E00010', 1, '負責領導，監督工作，與客戶溝通');
 
 --
--- 已傾印資料表的索引
+-- Indexes for dumped tables
 --
 
 --
--- 資料表索引 `employee`
+-- Indexes for table `employee`
 --
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`emp_id`);
 
 --
--- 資料表索引 `project`
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`loc_pid`);
+
+--
+-- Indexes for table `project`
 --
 ALTER TABLE `project`
   ADD PRIMARY KEY (`P_id`);
 
 --
--- 資料表索引 `project_member`
+-- Indexes for table `project_member`
 --
 ALTER TABLE `project_member`
-  ADD PRIMARY KEY (`emp_id`) USING BTREE,
-  ADD KEY `Pid` (`Pid`);
+  ADD PRIMARY KEY (`Pid`,`emp_id`),
+  ADD KEY `Prj_mem empid` (`emp_id`);
 
 --
--- 已傾印資料表的限制式
+-- Constraints for dumped tables
 --
 
 --
--- 資料表的限制式 `project_member`
+-- Constraints for table `location`
+--
+ALTER TABLE `location`
+  ADD CONSTRAINT `location_ibfk_1` FOREIGN KEY (`loc_pid`) REFERENCES `project` (`P_id`);
+
+--
+-- Constraints for table `project_member`
 --
 ALTER TABLE `project_member`
+  ADD CONSTRAINT `Prj_mem empid` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`),
   ADD CONSTRAINT `Prj_mem pid` FOREIGN KEY (`Pid`) REFERENCES `project` (`P_id`);
 COMMIT;
 
