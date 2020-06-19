e96559987a418818756c7b7577706cc69d821ff1
Gamma Toast <will890418@gmail.com>
2020-06-20 03:50:33 +0800

    Update mgr_cal emp_cal, 網站間的連結

---
+++ source/src/emp_cal.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+// Create connection
+$conn = mysqli_connect("localhost", $_SESSION["ac"], $_SESSION["pw"], "new_scs");
+$ac=$_SESSION["ac"];
+// Check connection
+if (empty($conn)){
+    session_destroy();
+    echo "無法連結資料庫";
+    die('<br><form style="text-align: center;" 
+          action="./index.html" method="post">
+          <input type="submit" value="返回首頁"/></form>');
+    exit;
+}
+$query = " SELECT * FROM emp_proj WHERE emp_id = '$ac' ORDER BY P_id ";
+$result = mysqli_query($conn, $query);
+$query2 = " SELECT * FROM emp_proj WHERE emp_id = '$ac' ORDER BY P_id ";
+$result2 = mysqli_query($conn, $query2);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>首頁(主管)|行事曆</title>
+<link href="./packages/core/main.css" rel="stylesheet" />
+<link href="./packages/daygrid/main.css" rel="stylesheet" />
+<link href="./packages/timegrid/main.css" rel="stylesheet" />
+<link href="./packages/list/main.css" rel="stylesheet" />
+<link href="./packages-premium/timeline/main.css" rel="stylesheet" />
+<link href="./packages-premium/resource-timeline/main.css" rel="stylesheet" />
+<script src="./packages/core/main.js"></script>
+<script src="./packages/interaction/main.js"></script>
+<script src="./packages/daygrid/main.js"></script>
+<script src="./packages/timegrid/main.js"></script>
+<script src="./packages/list/main.js"></script>
+<script src="./packages-premium/timeline/main.js"></script>
+<script src="./packages-premium/resource-common/main.js"></script>
+<script src="./packages-premium/resource-timeline/main.js"></script>
+<script>
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'resourceTimeline' ],
+      now: '<?php echo date("Y-m-d") ?>',
+      editable: true, // enable draggable events
+      aspectRatio: 1.8,
+      scrollTime: '00:00', // undo default 6am scrollTime
+      header: {
+        left: 'today prev,next',
+        center: 'title',
+        right: 'resourceTimelineDay,resourceTimelineThreeDays,timeGridWeek,dayGridMonth'
+      },
+      defaultView: 'resourceTimelineDay',
+      views: {
+        resourceTimelineThreeDays: {
+          type: 'resourceTimeline',
+          duration: { days: 3 },
+          buttonText: '3 days'
+        }
+      },
+      resourceLabelText: 'Rooms',
+      resources: [
+        <?php  
+          while($row = mysqli_fetch_array($result)) {
+				?>
+            { id: '<?php echo $row["P_id"] ?>', title: '<?php echo $row["loc_addr"]?>' },
+        <?php  
+          }  
+        ?>
+      ],
+      events: [
+        <?php  
+          while($row = mysqli_fetch_array($result2)) {
+            
+				?>
+            { id: '', resourceId: '<?php echo $row["P_id"] ?>', start: '<?php echo date("Y-m-d\TH:i:s",strtotime($row["start_date"])) ?>', end: '<?php echo date("Y-m-d\TH:i:s",strtotime($row["end_date"])) ?>', title: '<?php echo $row["P_name"] ?>' },
+        <?php  
+          }  
+        ?>
+      ]
+    });
+
+    calendar.render();
+  });
+
+</script>
+<style>
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  #calendar {
+    max-width: 900px;
+    margin: 50px auto;
+  }
+
+</style>
+</head>
+<body>
+
+  <div id="calendar"></div>
+
+</body>
+</html>
---
+++ source/src/emp_home.php
@@ -0,0 +1,81 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+        <head>
+            <title>首頁(員工)|員工派遣系統</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device width, initial-scale=1">
+            <link rel="stylesheet" href="css/w3school.css">
+            <link rel="stylesheet" href="css/emp_home.css">
+            <link rel="stylesheet" href="css/sidebar.css">
+        </head>
+        <body>
+        <!--Sidebar-->
+        <?php
+        //登入資料庫
+        $DBHOST = "localhost"; //主機位置
+        $conn = mysqli_connect( $DBHOST,$_SESSION["ac"],$_SESSION["pw"]); //連接資料庫
+        if (empty($conn)){
+            session_destroy();
+            echo "無法連結資料庫";
+            die('<br><form style="text-align: center;" 
+                  action="./index.html" method="post">
+                  <input type="submit" value="返回首頁"/></form>');
+            exit;
+        }
+        ?>
+        <div class="w3-sidebar w3-bar-block w3-collapse w3-card" style="width:200px;" id="mySidebar">
+            <button class="w3-bar-item w3-button w3-large w3-hide-large" onclick="w3_close()">Close &times;</button>
+            <a href="emp_home.php" class="w3-bar-item side_header w3-deep-purple">首頁</a>
+            <nav class="sidebar-nav">
+                <ul>
+                    <li>
+                        <a href="emp_project.php"><img src="img\project_main.png"><span class="">專案列表</span></a><!--href要改!!-->
+                    </li>
+                    <li>
+                        <a href="emp_cal.php"><img src="img\calender.png"><span class="">Cocaine</span></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+         <!-- Head -->
+        <div class="w3-main" style="margin-left:200px">
+            <div class="w3-deep-purple">
+                <!--nav-menu-button-->
+                <button class="w3-button w3-deep-purple w3-xlarge w3-hide-large" onclick="w3_open()">&#9776;</button>
+                <!--Content-title-->
+                <div class="w3-container">
+                    <h1>早安你好！ <span> [GET ID]</span></h1> <!--GET-USER-ID-->
+                </div>
+            </div>
+            <div class="w3-container">
+                <h2>專案</h2>
+                <div class="columns">
+                    <div class="block-button button-style1">
+                        <a href="emp_project.php" class="button" id=""><!--emp_project-->
+                          <span class="ico">
+                            <img class="icoh_back" src="img\project_main_hv2.png">
+                            <img class="icoh_front" src="img\project_main.png" width="55px">
+                          </span>
+                        </a>
+                        <h4 >專案列表</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+            function w3_open() {
+              document.getElementById("mySidebar").style.display = "block";
+            }
+    
+            function w3_close() {
+              document.getElementById("mySidebar").style.display = "none";
+            }
+            </script>
+
+
+        </body>
+</html>
+
